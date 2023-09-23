ff4f061bbee24d136203e7c002f35cc5395c505d
Tiffani Underwood <tdunderwood217@gmail.com>
2023-09-23 14:08:48 -0400

    Added current date to scheduler heading

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// #currentDay = ("current day"); {
+// const date = new Date();
+// console.log(date);
+// const todaysDate = date.toLocaleDateString();
+// console.log(todaysDate)
+// }
+
 $(function () {
+  const calendar = document.getElementById("currentDay");
+  const date = new Date();
+console.log(date);
+const todaysDate = date.toLocaleDateString();
+console.log(todaysDate)
+calendar.textContent = todaysDate;
+
+// var addTextEntry; (."row time-block") {
+console.log(localStorage);
+localStorage.setItem();
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
